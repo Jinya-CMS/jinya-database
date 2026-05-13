20cd00739bae597c4bb4530ef9db5d52d6f225a7
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2026-05-14 00:39:48 +0200

    Chore: Update docs startpage design

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,44 +1,45 @@
-import type {ReactNode} from 'react';
+import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
+import HomepageFeatures from "../components/HomepageFeatures/index";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Get Started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className={clsx('container', styles.container)}>
+                <div className="row">
+                    <div className="container">
+                        <h1 className="hero__title">{siteConfig.title}</h1>
+                        <p className="hero__subtitle">{siteConfig.tagline}</p>
+                        <div className={styles.buttons}>
+                            <Link
+                                className="button button--secondary button--lg"
+                                to="/docs/intro">
+                                Get started with Jinya Database
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <HomepageFeatures/>
+                </div>
+            </div>
+        </header>
+    );
 }
 
-export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+export default function Home() {
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={siteConfig.title}
+            description={siteConfig.tagline}>
+            <HomepageHeader/>
+        </Layout>
+    );
 }
